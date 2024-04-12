67becf6ebc2c2513215fdaca8e7b423f248c0bb0
ali <adadashipour@gmail.com>
2024-04-12 18:08:36 +0100

    more loadUI() and clickable boxes being made

--- src/blank-load.js
+++ src/blank-load.js
@@ -1,4 +1,7 @@
+import './style.css';
+
 //Factory function to create blank project array list
+
 export const loadBlankProject = () => {
     console.log('Called LoadBlankProject - Creating a blank project array...');
     let projectsArray = [];
@@ -12,4 +15,15 @@ export const loadBlankProject = () => {
 export const loadUI = () => {
     let content = document.getElementById('content');
     content.classList.add('box');
+
+    document.addEventListener("DOMContentLoaded", function() {
+        // Select the plus sign element
+        var plusSign = document.querySelector(".plus-sign");
+      
+        // Add click event listener to the plus sign
+        plusSign.addEventListener("click", function() {
+          // Handle click event here, for example, log a message
+          console.log("Plus sign clicked!");
+        });
+      });
 }
