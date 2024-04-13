c3ac4ef62f642b74ef66ff48177761171c5e364c
ali <adadashipour@gmail.com>
2024-04-13 17:03:10 +0100

    New logic adds new todo while overwriting the existing todo box with actual todo content

--- src/ui.js
+++ src/ui.js
@@ -1,6 +1,7 @@
 function createBox() {
     const box = document.createElement('div');
     box.classList.add('box');
+    box.textContent = 'Click me to add todo'; // Default content
 
     const plusSign = document.createElement('div');
     plusSign.textContent = '+';
@@ -10,13 +11,18 @@ function createBox() {
     return { box, plusSign };
 }
 
+function initialiseTodoBox(box) {
+    box.textContent = 'Todo List'; // Update content to todo list
+    box.classList.remove('clickable'); // Remove clickable class
+}
+
 function addTodoBox() {
     const content = document.querySelector('.content');
     const { box, plusSign } = createBox();
 
     plusSign.addEventListener('click', function() {
-        console.log('Plus sign clicked!');
-        addTodoBox();
+        initialiseTodoBox(box); // Initialise clicked box as todo list
+        addTodoBox(); // Add a new clickable box
     });
 
     content.appendChild(box);
@@ -26,9 +32,11 @@ export const loadInitialUI = () => {
     const content = document.querySelector('.content');
     const { box, plusSign } = createBox();
 
+    plusSign.classList.add('clickable'); // Add clickable class to initial box
+
     plusSign.addEventListener('click', function() {
-        console.log('Plus sign clicked!');
-        addTodoBox();
+        initialiseTodoBox(box); // Initialise clicked box as todo list
+        addTodoBox(); // Add a new clickable box
     });
 
     content.appendChild(box);
