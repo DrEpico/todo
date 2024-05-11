c87a82b2f5956fbfd2f9068933e5d194599a2de5
ali <adadashipour@gmail.com>
2024-05-11 13:25:49 +0100

    Fixing webpack errors

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,41 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  resolve: {
+    fallback: {
+      "os": require.resolve("os-browserify/browser"),
+      "fs": false, // You can choose to set it to false if not needed
+      "http": require.resolve("stream-http"),
+      "https": require.resolve("https-browserify"),
+      "util": require.resolve("util/"),
+      "path": require.resolve("path-browserify"),
+      "crypto": require.resolve("crypto-browserify"),
+      "stream": require.resolve("stream-browserify"),
+      "buffer": require.resolve('buffer/'),
+        "url": require.resolve('url/'),
+        "vm": require.resolve('vm-browserify'),
+        "zlib": require.resolve('browserify-zlib'),
+      "assert": require.resolve("assert/"),
+      "constants": require.resolve("constants-browserify"),
+      "querystring": require.resolve("querystring-es3"),
+      'child_process': require.resolve('child_process'),
+      'tty': require.resolve('tty-browserify'),
+      'workers': require.resolve('worker_threads')
+    }
+  },
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.node$/,
+        use: 'node-loader', // or 'file-loader' if you prefer
+      },
+      {
+        test: /\.d\.ts$/, // Handle TypeScript definition files
+        use: 'ignore-loader', // Ignore TypeScript definition files
       },
     ],
   },
