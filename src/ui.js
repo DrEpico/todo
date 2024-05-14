24079cae550d79448ba3f62c373b6726019c6a94
ali <adadashipour@gmail.com>
2024-05-14 13:48:16 +0100

    Added text boxes for user input

--- src/ui.js
+++ src/ui.js
@@ -18,26 +18,26 @@ function initTodoBox(box, todo) {
     box.textContent = 'Todo List'; // Update content to todo list
     box.classList.remove('clickable'); // Remove clickable class
 
-    const titleElement = document.createElement('h2');
-    titleElement.textContent = todo.title;
+    // const titleElement = document.createElement('h2');
+    // titleElement.textContent = todo.title;
 
-    const descriptionElement = document.createElement('p');
-    descriptionElement.textContent = `Description: ${todo.description}`;
+    // const descriptionElement = document.createElement('p');
+    // descriptionElement.textContent = `Description: ${todo.description}`;
 
-    const dueDateElement = document.createElement('p');
-    dueDateElement.textContent = `Due Date: ${todo.dueDate}`
+    // const dueDateElement = document.createElement('p');
+    // dueDateElement.textContent = `Due Date: ${todo.dueDate}`
 
-    const priorityElement = document.createElement('p');
-    priorityElement.textContent = `Priority: ${todo.priority}`;
+    // const priorityElement = document.createElement('p');
+    // priorityElement.textContent = `Priority: ${todo.priority}`;
     
-    const checklistElement = document.createElement('p');
-    checklistElement.textContent = `Status: ${todo.checklist ? 'Due' : 'Done'}`;
+    // const checklistElement = document.createElement('p');
+    // checklistElement.textContent = `Status: ${todo.checklist ? 'Due' : 'Done'}`;
 
-    box.appendChild(titleElement);
-    box.appendChild(descriptionElement);
-    box.appendChild(dueDateElement);
-    box.appendChild(priorityElement);
-    box.appendChild(checklistElement);
+    // box.appendChild(titleElement);
+    // box.appendChild(descriptionElement);
+    // box.appendChild(dueDateElement);
+    // box.appendChild(priorityElement);
+    // box.appendChild(checklistElement);
 }
 
 function addTodoBox() {
@@ -72,33 +72,73 @@ export const loadInitialUI = () => {
 };
 
 function generateForm(box){
+    let form = document.createElement("form");
+    let lineBreak = document.createElement("br");
+    let titleLabel = document.createElement("label");
+    titleLabel.setAttribute("for", "title");
+    titleLabel.textContent = "Todo:         "
     let titleElement = document.createElement("input");
     titleElement.setAttribute("type", "text");
     titleElement.setAttribute("id", "title");
+    titleElement.setAttribute("name", "title");
+
+    let descriptionLabel = document.createElement("label");
+    descriptionLabel.setAttribute("for", "description");
+    descriptionLabel.textContent = "Description:    ";
     let descriptionElement = document.createElement("input");
     descriptionElement.setAttribute("type", "text");
     descriptionElement.setAttribute("id", "description");
+    descriptionElement.setAttribute("name", "description");
+
+    let dueDateLabel = document.createElement("label");
+    dueDateLabel.setAttribute("for", "dueDate");
+    dueDateLabel.textContent = "Due:        ";
     let dueDateElement = document.createElement("input");
     dueDateElement.setAttribute("type", "date");
     dueDateElement.setAttribute("id", "dueDate");
+    dueDateElement.setAttribute("name", "dueDate");
+    
+    let priorityLabel = document.createElement("label");
+    priorityLabel.setAttribute("for", "priority");
+    priorityLabel.textContent = "Priority: ";
     let priorityElement = document.createElement("input");
     priorityElement.setAttribute("type", "text");
     priorityElement.setAttribute("id", "priority");
+    priorityElement.setAttribute("name", "priority");
+
+    let checklistLabel = document.createElement("label");
+    checklistLabel.setAttribute("for", "checklis");
+    checklistLabel.textContent = "Due : Done";
     let checklistElement = document.createElement("input");
     checklistElement.setAttribute("type", "text");
     checklistElement.setAttribute("id", "checklist");
+    checklistElement.setAttribute("name", "checklist");
     
     let submitButton = document.createElement("button");
     submitButton.setAttribute("id", "createBtn");
     submitButton.textContent = "Create";
 
-    
-    box.appendChild(titleElement);
-    box.appendChild(descriptionElement);
-    box.appendChild(dueDateElement);
-    box.appendChild(descriptionElement);
-    box.appendChild(priorityElement);
-    box.appendChild(checklistElement);
-    box.appendChild(submitButton);
+    form.appendChild(titleLabel);
+    form.appendChild(document.createElement("br"));
+    form.appendChild(titleElement);
+    form.appendChild(document.createElement("br"));
+    form.appendChild(descriptionLabel);
+    form.appendChild(document.createElement("br"));
+    form.appendChild(descriptionElement);
+    form.appendChild(document.createElement("br"));
+    form.appendChild(priorityLabel);
+    form.appendChild(document.createElement("br"));
+    form.appendChild(priorityElement);
+    form.appendChild(document.createElement("br"));
+    form.appendChild(checklistLabel);
+    form.appendChild(document.createElement("br"));
+    form.appendChild(checklistElement);
+    form.appendChild(document.createElement("br"));
+    form.appendChild(dueDateLabel);
+    form.appendChild(document.createElement("br"));
+    form.appendChild(dueDateElement);
+    form.appendChild(document.createElement("br"));
+    form.appendChild(submitButton);
+    box.appendChild(form);
 }
 
